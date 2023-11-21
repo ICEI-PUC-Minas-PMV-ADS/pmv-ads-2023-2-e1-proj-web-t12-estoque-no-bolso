c09055549a8494efeb32fdb010c86fbb46938d63
Matheus Castelliano <matheus_castelliano@outlook.com>
2023-11-21 00:08:49 -0300

    ajeitando o JS do formulario

--- codigo-fonte/Página Padrão/modais-estoque/pag_estoque.js
+++ codigo-fonte/Página Padrão/modais-estoque/pag_estoque.js
@@ -1,91 +1,99 @@
-document.addEventListener('DOMContentLoaded', function () {
-   
-  // Recuperar os dados dos PRODUTOS do localStorage
-  const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
+document.addEventListener('DOMContentLoaded', function() {
   
-  document
-      .querySelector("#contact-form")
-      .addEventListener("submit", function (e) {
-        e.preventDefault();
-
-        // Coletar os dados do formulário
-        const nome = document.querySelector("#nome").value;
-        const email = document.querySelector("#email").value;
-        const telefone = document.querySelector("#telefone").value;
-        const mensagem = document.querySelector("#mensagem").value;
-        const whatsapp = document.querySelector("#whatsapp").value;
-
-        // Verificar se o nome e o email foram preenchidos
-        if (nome && email) {
-          // Criar um objeto de contato com um ID único
-          const produto = {
-            id: idCounter, // Use o contador como ID
-            nome,
-            email,
-            telefone,
-            whatsapp,
-            mensagem,
-          };
-
-          // Incrementar o contador para o próximo ID único
-          idCounter++;
-
-          // Adicionar o contato à lista
-          produtos.push(produto);
-
-          // Salvar os contatos no localStorage
-          localStorage.setItem("produtos", JSON.stringify(produtos));
-
-          // Limpar o formulário
-          document.querySelector("#nome").value = "";
-          document.querySelector("#email").value = "";
-          document.querySelector("#telefone").value = "";
-          document.querySelector("#whatsapp").value = "";
-          document.querySelector("#mensagem").value = "";
-
-          // Exibir uma mensagem de confirmação
-          document.querySelector("#confirmation-message").textContent =
-            "Contato salvo com sucesso!";
-        }
-
+    // Recuperar os dados dos PRODUTOS do localStorage
+    const produtos = JSON.parse(localStorage.getItem("produtos")) || [];
   
-  
-
-// MODAL
-
-  const openModalBtn = document.getElementById('openModalBtn');
-    const closeModalBtn = document.getElementById('closeModalBtn');
-    const modal = document.getElementById('myModal');
-    const itemForm = document.getElementById('itemForm');
-  
-    openModalBtn.addEventListener('click', function () {
-      modal.style.display = 'block';
+    // Associar salvamento ao botão
+    document.querySelector('#btn_salvar').addEventListener('click', function(e) {
+      salvaProduto(e);
+      console.log('btn_salvar');
     });
   
-    closeModalBtn.addEventListener('click', function () {
-      modal.style.display = 'none';
-    });
-  
-    itemForm.addEventListener('submit', function (e) {
+    function salvaProduto(e) {
       e.preventDefault();
-  
-      const newItem = {
-        name: document.getElementById('itemName').value,
-        price: document.getElementById('itemPrice').value,
-        cost: document.getElementById('itemCost').value,
-        quantity: document.getElementById('itemQuantity').value,
-        id: document.getElementById('itemID').value,
-        size: document.getElementById('itemSize').value,
-        description: document.getElementById('itemDescription').value,
-        category: document.getElementById('itemCategory').value,
-        image: document.getElementById('itemImage').value, // Aqui você pode salvar o caminho ou a imagem em base64
-      };
-  
-      let itemList = JSON.parse(localStorage.getItem('itemList')) || [];
-      itemList.push(newItem);
-      localStorage.setItem('itemList', JSON.stringify(itemList));
-  
-      modal.style.display = 'none';
-    });
+    
+      // Coletar os dados do formulário
+      let cod = document.querySelector('#txt_cod').value;
+      let produto = document.querySelector('#txt_produto').value;
+      let categoria = document.querySelector('#txt_categoria').value;
+      let variacao = document.querySelector('#txt_variacao').value;
+      let custo = document.querySelector('#txt_custo').value;
+      let quantidade = document.querySelector('#txt_quantidade').value;
+      let preco = document.querySelector('#txt_preco').value;
+      let descricao = document.querySelector('#txt_descricao').value;
+    
+      // Verificar se o nome e o email foram preenchidos
+      if (produto && categoria && quantidade) {
+        // Criar um objeto de produto com um ID único
+        const produtoObj = {
+          cod,
+          produto,
+          categoria,
+          variacao,
+          custo,
+          quantidade,
+          preco,
+          descricao,
+        };
+    
+        // Adicionar o produto à lista
+        produtos.push(produtoObj);
+    
+        // Salvar os produtos no localStorage
+        localStorage.setItem('produtos', JSON.stringify(produtos));
+      }
+    }
   });
-}
+  
+      
+        //   // Limpar o formulário
+        //   document.querySelector("#nome").value = "";
+        //   document.querySelector("#email").value = "";
+        //   document.querySelector("#telefone").value = "";
+        //   document.querySelector("#whatsapp").value = "";
+        //   document.querySelector("#mensagem").value = "";
+
+        //   // Exibir uma mensagem de confirmação
+        //   document.querySelector("#confirmation-message").textContent =
+        //     "Contato salvo com sucesso!";
+        // }
+  
+
+  //   // MODAL
+
+  //   const openModalBtn = document.querySelector('openModalBtn');
+  //   const closeModalBtn = document.querySelector('closeModalBtn');
+  //   const modal = document.querySelector('myModal');
+  //   const itemForm = document.querySelector('itemForm');
+  
+  //   openModalBtn.addEventListener('click', function () {
+  //     modal.style.display = 'block';
+  //   });
+  
+  //   closeModalBtn.addEventListener('click', function () {
+  //     modal.style.display = 'none';
+  //   });
+  
+  //   itemForm.addEventListener('submit', function (e) {
+  //     e.preventDefault();
+  
+  //     const newItem = {
+  //       name: document.querySelector('itemName').value,
+  //       price: document.querySelector('itemPrice').value,
+  //       cost: document.querySelector('itemCost').value,
+  //       quantity: document.querySelector('itemQuantity').value,
+  //       id: document.querySelector('itemID').value,
+  //       size: document.querySelector('itemSize').value,
+  //       description: document.querySelector('itemDescription').value,
+  //       category: document.querySelector('itemCategory').value,
+  //       image: document.querySelector('itemImage').value, // Aqui você pode salvar o caminho ou a imagem em base64
+  //     };
+  
+  //     let itemList = JSON.parse(localStorage.getItem('itemList')) || [];
+  //     itemList.push(newItem);
+  //     localStorage.setItem('itemList', JSON.stringify(itemList));
+  
+  //     modal.style.display = 'none';
+  //   });
+  // });
+ 
