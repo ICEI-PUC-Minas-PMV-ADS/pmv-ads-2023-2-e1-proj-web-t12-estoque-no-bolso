66d661dcbdb0aa2c9d67b1c2a12bc1b750e58956
Fagner Passos <fagner.passos@outlook.com>
2023-11-25 12:30:40 -0300

    Finalizei a parte de estoque.

--- codigo-fonte/Página Padrão/modais-estoque/pag_estoque.js
+++ codigo-fonte/Página Padrão/modais-estoque/pag_estoque.js
@@ -1,96 +1,142 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const produtos = []; // Array para armazenar os produtos
+document.addEventListener("DOMContentLoaded", function () {
+  let produtos = []; // Array para armazenar os produtos
+  let editingIndex = -1; // Índice do produto em edição
 
-    const modalProdutos = new bootstrap.Modal(document.getElementById('modalProdutos'), {
-        backdrop: 'static'
-    });
-
-    const formProdutos = document.getElementById('form-produtos');
-    const btnSalvar = document.getElementById('btn_salvar');
-    const tabelaProdutos = document.getElementById('tabelaPrudotosVendas');
-
-    // Função para limpar o formulário
-    function clearForm() {
-        formProdutos.reset();
+  // Função para carregar os dados do localStorage
+  function loadFromLocalStorage() {
+    const storedData = localStorage.getItem("produtos");
+    if (storedData) {
+      produtos = JSON.parse(storedData);
+      renderTable();
     }
+  }
+  const formProdutos = document.getElementById("form-produtos");
+  const btnSalvar = document.getElementById("btn_salvar");
+  const tabelaProdutos = document.getElementById("tabelaPrudotosVendas");
 
-    // Função para renderizar a tabela de produtos
-    function renderTable() {
-        tabelaProdutos.innerHTML = '';
+  loadFromLocalStorage();
 
-        for (let i = 0; i < produtos.length; i++) {
-            const produto = produtos[i];
+  // Função para renderizar a tabela de produtos
+  function renderTable() {
+    tabelaProdutos.innerHTML = "";
 
-            const row = document.createElement('tr');
-            row.innerHTML = `<td><input type="checkbox" class="form-check-input"></td>
+    for (let i = 0; i < produtos.length; i++) {
+      const produto = produtos[i];
+
+      const row = document.createElement("tr");
+      row.innerHTML = `<td><input type="checkbox" class="form-check-input"></td>
                             <td>${produto.id}</td>
                             <td>${produto.nome}</td>
-                            <td>${produto.estoque}</td>
+                            <td>${produto.categoria}</td>
+                            <td>${produto.tamanho}</td>
                             <td>${produto.custo}</td>
-                            <td>${produto.preco}</td>`;
+                            <td>${produto.quantidade}</td>
+                            <td>${produto.valor}</td>
+                            <td>${produto.descricao}</td>
+                            <td>
+                                <button class="btn-editar" data-index="${i}">Editar</button>
+                                <button class="btn-excluir" data-index="${i}">Excluir</button>
+                            </td>`;
 
-            tabelaProdutos.appendChild(row);
-        }
+      tabelaProdutos.appendChild(row);
     }
 
-    // Evento do botão "Adicionar"
-    const btnAdicionar = document.getElementById('btn_adicionar');
-    if (btnAdicionar) {
-        btnAdicionar.addEventListener('click', function () {
-            clearForm();
-            modalProdutos.show();
-        });
+    // Salva os dados no localStorage após renderizar a tabela
+    saveToLocalStorage();
+  }
+
+  // Função para salvar os dados no localStorage
+  function saveToLocalStorage() {
+    localStorage.setItem("produtos", JSON.stringify(produtos));
+  }
+
+  // Função para limpar o formulário
+  function clearForm() {
+    document.getElementById("txt_cod").value = "";
+    document.getElementById("txt_produto").value = "";
+    document.getElementById("txt_categoria").value = "";
+    document.getElementById("txt_tamanho").value = "";
+    document.getElementById("txt_custo").value = "";
+    document.getElementById("txt_quantidade").value = "";
+    document.getElementById("text_venda").value = "";
+    document.getElementById("text_descricao").value = "";
+  }
+
+  // Evento do botão "Salvar"
+  btnSalvar.addEventListener("click", function () {
+    const id = document.getElementById("txt_cod").value;
+    const nome = document.getElementById("txt_produto").value;
+    const categoria = document.getElementById("txt_categoria").value;
+    const tamanho = document.getElementById("txt_tamanho").value;
+    const custo = document.getElementById("txt_custo").value;
+    const quantidade = document.getElementById("txt_quantidade").value;
+    const valor = document.getElementById("text_venda").value;
+    const descricao = document.getElementById("text_descricao").value;
+
+    if (editingIndex === -1) {
+      // Adiciona um novo produto se não estiver em modo de edição
+      produtos.push({
+        id,
+        nome,
+        categoria,
+        tamanho,
+        custo,
+        quantidade,
+        valor,
+        descricao,
+      });
+    } else {
+      // Edita o produto se estiver em modo de edição
+      produtos[editingIndex] = {
+        id,
+        nome,
+        categoria,
+        tamanho,
+        custo,
+        quantidade,
+        valor,
+        descricao,
+      };
+      editingIndex = -1;
     }
 
-    // Evento do botão "Salvar"
-    btnSalvar.addEventListener('click', function () {
-        // Adicione aqui a lógica para salvar o produto no array
-        // Exemplo:
-        const novoProduto = {
-            id: document.getElementById('txt_cod').value,
-            nome: document.getElementById('txt_produto').value,
-            // Adicione os outros campos conforme necessário
-        };
+    // Renderiza a tabela novamente
+    renderTable();
 
-        produtos.push(novoProduto);
+    // Limpa o formulário e fecha o modal
+    clearForm();
+    $("#modalProdutos").modal("hide");
+  });
 
-        // Renderiza a tabela novamente
-        renderTable();
+  // Evento da tabela para abrir modal de edição ou excluir
+  tabelaProdutos.addEventListener("click", function (event) {
+    const target = event.target;
 
-        // Limpa o formulário e fecha o modal
-        clearForm();
-        modalProdutos.hide();
-    });
+    if (target.classList.contains("btn-editar")) {
+      // Modo de edição
+      const index = target.dataset.index;
+      editingIndex = index;
 
-    // Evento do botão "Excluir"
-    document.getElementById('btn_excluir').addEventListener('click', function () {
-        // Adicione aqui a lógica para excluir os produtos selecionados
-        // Exemplo:
-        const checkboxes = document.querySelectorAll('#tabelaPrudotosVendas input[type="checkbox"]:checked');
+      document.getElementById("txt_cod").value = produtos[index].id;
+      document.getElementById("txt_produto").value = produtos[index].nome;
+      document.getElementById("txt_categoria").value =
+        produtos[index].categoria;
+      document.getElementById("txt_tamanho").value = produtos[index].tamanho;
+      document.getElementById("txt_custo").value = produtos[index].custo;
+      document.getElementById("txt_quantidade").value =
+        produtos[index].quantidade;
+      document.getElementById("text_venda").value = produtos[index].valor;
+      document.getElementById("text_descricao").value =
+        produtos[index].descricao;
 
-        for (let i = checkboxes.length - 1; i >= 0; i--) {
-            const index = checkboxes[i].parentNode.parentNode.rowIndex - 1;
-            produtos.splice(index, 1);
-        }
+      $("#modalProdutos").modal("show");
+    } else if (target.classList.contains("btn-excluir")) {
+      // Excluir o produto
+      const index = target.dataset.index;
+      produtos.splice(index, 1);
 
-        // Renderiza a tabela novamente
-        renderTable();
-    });
-
-    // Evento da tabela para abrir modal de edição
-    tabelaProdutos.addEventListener('click', function (event) {
-        const target = event.target;
-        if (target.tagName === 'TD' && target.parentNode.rowIndex > 0) {
-            const index = target.parentNode.rowIndex - 1;
-            const produtoSelecionado = produtos[index];
-
-            // Preencha o formulário com os dados do produto
-            document.getElementById('txt_cod').value = produtoSelecionado.id;
-            document.getElementById('txt_produto').value = produtoSelecionado.nome;
-            // Preencha os outros campos conforme necessário
-
-            // Abre o modal de edição
-            modalProdutos.show();
-        }
-    });
+      // Renderiza a tabela novamente
+      renderTable();
+    }
+  });
 });
