57de68531b3ccc9755e8c8d30d7a6cbd198b2461
Matheus Castelliano <matheus_castelliano@outlook.com>
2023-12-08 20:56:29 -0300

    Adicionei algumas partes no relatorio_estoque.js

--- codigo-fonte/Página Padrão/relatorio_mariana/relatorio_estoque.js
+++ codigo-fonte/Página Padrão/relatorio_mariana/relatorio_estoque.js
@@ -49,6 +49,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+
+    // ATUALIZA A LISTA DE PRODUTOS VENDIDOS
+    function atualizarListaProdutos() {
+        const tabelaDeProdutos = document.querySelector("#tabelaProdutosVendidos");
+        const registrosExistente = JSON.parse(localStorage.getItem("registroVendas")) || [];
+    
+        // Preencher a tabela com os produtos
+        registrosExistente.forEach(function (produto, index) {
+            const row = tabelaDeProdutos.insertRow();
+    
+            const cellCod = row.insertCell(0);
+            cellCod.textContent = produto.cod;
+    
+            const cellNome = row.insertCell(1);
+            cellNome.textContent = produto.nome;
+    
+            const cellEstoque = row.insertCell(2);
+            cellEstoque.textContent = produto.quantidade;
+    
+            const cellCusto = row.insertCell(3);
+            cellCusto.textContent = produto.custo;
+    
+            const cellvalorVenda = row.insertCell(4);
+            cellvalorVenda.textContent = produto.valorVenda;
+        });
+    }
+    
+
+    
+
     // ADICIONAR O ITEM SELECIONADO NO CARRINHO
     function adicionarNoCarrinho(botao, produto) {
         const carrinho = document.querySelector("#tabelaPrudotosVendas");
